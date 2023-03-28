23e92a3dd404d8c6ac97bc99583fcf59f969bebb
Laisa <laisa.garlini@hotmail.com>
2023-03-28 16:17:58 -0300

    nova-estrutura

--- index.php
+++ index.php
@@ -1,21 +1,83 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sistema de Contatos</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
 </head>
 <body>
-    <?php
+  <div class="pessoas">
+    <div class="cabeçalho">
+      <label for="nome">Nome:</label>
+      <input type="text" id="nome" name="nome">
+
+      <button type="button" class="pesquisar-pessoa">Pesquisar</button>
+      <button type="button" class="add-pessoa">Adicionar Pessoa</button>
+    </div>
+
+    <table>
+		<tr>
+			<th>ID</th>
+			<th>Nome</th>
+			<th>CPF</th>
+		</tr>
+		<?php
       include("autoload.class.php");
 
-      $ConexaoPG = new Conexao("localhost","contatos","postgres","");
-
+      $ConexaoPG = new Conexao("localhost","postgres","postgres","postgres");
+      
       $resultado = $ConexaoPG->query("SELECT * FROM pessoa");
-        foreach ($resultado as $row) {
-            echo $row['nome'] . "\n";
+      foreach ($resultado as $row) {
+          echo "<tr><td ondblclick='EditarRegistro(\"" . $row["id"] . "\")'>" . $row["id"]. "</td><td ondblclick='EditarRegistro(\"" . $row["id"] . "\")'>" . $row["nome"]. "</td><td ondblclick='EditarRegistro(\"" . $row["id"] . "\")'>" . $row["cpf"]. "</td></tr>";
       }
-    ?>
+		?>
+	</table>
+
+    <div id="modal" class="modal">
+        <div class="modal-conteudo">
+            <h2>Editar</h2>
+            <form id="formulario">
+            <label for="Nome">Nome:</label>
+            <input type="text" id="Nome" name="Nome">
+
+            <label for="cpf">CPF:</label>
+            <input type="text" id="cpf" name="cpf">
+
+            <button type="submit" class="salvar">Salvar</button>
+            <button type="button" class="deletar">Deletar Registro</button>
+            <button type="button" onclick="Sair()" class="cancelar">Cancelar</button>
+            </form>
+        </div>
+    </div>
+  </div>
+
+  <div class="contatos">
+  <div class="cabeçalho">
+      <label for="descricao">Descrição:</label>
+      <input type="text" id="descricao" name="descricao">
+
+      <button type="button" class="pesquisar-contato">Pesquisar</button>
+      <button type="button" class="add-contato">Adicionar Contato</button>
+    </div>
+
+    <table>
+		<tr>
+			<th>Nome</th>
+			<th>Tipo</th>
+			<th>Descrição</th>
+		</tr>
+		<?php
+      include("autoload.class.php");
+
+      $ConexaoPG = new Conexao("localhost","postgres","postgres","postgres");
+      
+      $resultado = $ConexaoPG->query("select contato.id, pessoa.nome, tipo, descricao from contato join pessoa on pessoa.id = contato.id_pessoa");
+      foreach ($resultado as $row) {
+          echo "<tr><td ondblclick='EditarRegistro(\"" . $row["id"] . "\")'>" . $row["nome"]. "</td><td ondblclick='EditarRegistro(\"" . $row["id"] . "\")'>" . $row["tipo"]. "</td><td ondblclick='EditarRegistro(\"" . $row["id"] . "\")'>" . $row["descricao"]. "</td></tr>";
+      }
+		?>
+	</table>
+  </div>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-function Pessoas() {
-    window.open("pessoas.html", "Pessoas");
+function Sair() {
+    var modal = document.getElementById('modal');
+    
+    modal.style.display = 'none';    
 }
 
-function Contatos() {
-    window.open("contato.html", "Contatos");
+function EditarRegistro(id) {
+    var modal = document.getElementById('modal');
+    
+    modal.style.display = 'flex';
+
+    // alert("Id: " + id);
+  }
+    
+function PesquisarPessoa() {
+    var nome = document.getElementsById('nome').value;
+    
+    modal.style.display = 'none';    
 }
+
+//   var formulario = document.getElementById('formulario');
+
+//   formulario.addEventListener('submit', function(event) {
+//     event.preventDefault();
+  
+//     // Faça a chamada AJAX para atualizar o registro no servidor
+//     // ...
+  
+//     // Feche o modal
+//     modal.style.display = 'none';
+//   });
+  
